3383deb75cc52f7e597fd1eb5a0ab8ffa71da16b
Rishikesh <kumarcrishikesh@gmail.com>
2024-04-17 22:48:21 -0400

    First Commit - hit an API and print response

---
+++ app/Main.hs
@@ -0,0 +1,13 @@
+#!/usr/bin/env stack
+{-# LANGUAGE OverloadedStrings #-}
+import qualified Data.ByteString.Lazy.Char8 as L8
+import           Network.HTTP.Simple
+
+main :: IO ()
+main = do
+    response <- httpLBS "http://127.0.0.1:5000"
+
+    putStrLn $ "The status code was: " ++
+               show (getResponseStatusCode response)
+    print $ getResponseHeader "Content-Type" response
+    L8.putStrLn $ getResponseBody response
