492178e24fdec964d26275f787c0bac492108194
Rishikesh <kumarcrishikesh@gmail.com>
2024-06-03 11:20:21 -0400

    switched to haskell-flake. Flake dockerization WIP

--- flake.nix
+++ flake.nix
@@ -1,53 +1,77 @@
-# SPDX-FileCopyrightText: 2021 Serokell <https://serokell.io/>
-#
-# SPDX-License-Identifier: CC0-1.0
-
 {
-  description = "My haskell application";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
   };
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = ["x86_64-linux"];
+      imports = [
+        inputs.haskell-flake.flakeModule
+      ];
+      perSystem = { self', system, lib, config, pkgs, ... }:{
+        haskellProjects.default = {
+                    
+          basePackages = pkgs.haskellPackages;
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-
-        haskellPackages = pkgs.haskellPackages;
-
-        jailbreakUnbreak = pkg:
-          pkgs.haskell.lib.doJailbreak (pkg.overrideAttrs (_: { meta = { }; }));
-
-        packageName = "eatsy-hs";
-      in {
-        packages.${packageName} =
-          haskellPackages.callCabal2nix packageName self rec {
-            scotty = jailbreakUnbreak haskellPackages.scotty_0_22;
+          packages = {
+            scotty.source = "0.22"; # Hackage version
           };
 
-        packages.default = self.packages.${system}.${packageName};
-        defaultPackage = self.packages.${system}.default;
-        
+          devShell = {
+            hlsCheck.enable = false;
+          };
+
+          autoWire = [ "packages" "apps" "checks" ]; 
+        };
+
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
+          name = "eatsy-hs devShell";
+          inputsFrom = [
+            config.haskellProjects.default.outputs.devShell
+          ];
+          nativeBuildInputs = with pkgs; [
             haskellPackages.haskell-language-server
             ghcid
             cabal-install
             zlib
             zlib.dev
-            dbeaver
+            dbeaver-bin
             beekeeper-studio
             haskellPackages.yesod-bin
             jq
+            azure-cli
           ];
           shellHook = ''
               export NEURELO_KEY=$(cat neurelo_key)
               export NEURELO_ENDPOINT="https://us-east-2.aws.neurelo.com"
           '';
-          inputsFrom = map (__getAttr "env") (__attrValues self.packages.${system});
         };
-        devShell = self.devShells.${system}.default;
-      });
+
+
+# WIP add envs
+        packages.dockerImage = pkgs.dockerTools.buildImage {
+          name = "eatsy-hs";
+          created = "now";
+          tag = builtins.substring 0 9 (self'.rev or "dev");
+          copyToRoot = pkgs.buildEnv {
+            paths = with pkgs; [
+              # writeNeureloKey
+              coreutils
+              bash
+            ];
+            name = "eatsy-hs-root";
+            pathsToLink = [ "/bin" "/secrets"];
+          };        
+          config = {
+            Cmd = [ "${pkgs.lib.getExe self'.packages.default}" ];
+          };
+        };
+      
+      packages.default = self'.packages.eatsy-hs;
+      apps.default = self'.apps.eatsy-hs;
+      
+      };
+    };
 }
